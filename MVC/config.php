1de03bd64d23b74ec68710176e583f42754c266d
adoptimize-as <42072310+adoptimize-as@users.noreply.github.com>
2022-05-26 14:23:19 +0200

    fix configs 3

--- MVC/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DB_PREFIX', "swiff.crm.");
-define('DB_PRODUCTION', "production");
-define('PUBLIC_DIR', BASIS_DIR.'/Public');
-define('PUBLIC_URL', BASIS_URL.'/Public');
--- index.php
+++ index.php
@@ -9,6 +9,5 @@ ini_set('display_errors', 1);
 
 require_once(__DIR__.'/config.php');
 
-#phpinfo();
 require_once './MVC/config_db.php';
 require_once './MVC/MVC.php';
