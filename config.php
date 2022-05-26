f7006fe035f7adc23e00008b9a19143f4e57afd6
adoptimize-as <42072310+adoptimize-as@users.noreply.github.com>
2022-05-26 14:11:35 +0200

    fix config 2

--- config.php
+++ config.php
@@ -7,8 +7,10 @@ define('BASIS_RECHNUNG_URL', BASIS_URL."/Public/Rechnungen");
 
 /*** PATH CONFIGS ***/
 define('BASIS_DIR', __DIR__ . DIRECTORY_SEPARATOR); //z.B. D:\www\tut
+define('PUBLIC_URL', BASIS_URL.'Public');
 define('TWIG_CACHE_DIR', BASIS_DIR . 'TemplatesTwigCache' . DIRECTORY_SEPARATOR);
 define('TWIG_TEMPLATE_DIR', BASIS_DIR . 'TemplatesTwig' . DIRECTORY_SEPARATOR);
 define('BASIS_RECHNUNG_DIR', BASIS_DIR . 'Public' . DIRECTORY_SEPARATOR . 'Rechnungen' . DIRECTORY_SEPARATOR);
 define('BLOGIC_DIR', BASIS_DIR . 'BLogic' . DIRECTORY_SEPARATOR);
+
 
