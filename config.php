1d5e7c2972ae5d02342852c5ec00997d3fe3ab38
adoptimize-as <42072310+adoptimize-as@users.noreply.github.com>
2022-05-26 14:39:56 +0200

    fix BASIS_URL

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
 
+$__tmp__ = explode($_SERVER['DOCUMENT_ROOT'], __DIR__);
+$__relativeBasePath__ = end($__tmp__);
+
 /*** URL CONFIGS */
-define('BASIS_URL', "");
+define('BASIS_URL', $__relativeBasePath__);
 define('PUBLIC_URL', BASIS_URL . '/Public');
 define('BASIS_RECHNUNG_URL', BASIS_URL."/Public/Rechnungen");
 
@@ -12,3 +15,6 @@ define('TWIG_CACHE_DIR', BASIS_DIR . 'TemplatesTwigCache' . DIRECTORY_SEPARATOR)
 define('TWIG_TEMPLATE_DIR', BASIS_DIR . 'TemplatesTwig' . DIRECTORY_SEPARATOR);
 define('BASIS_RECHNUNG_DIR', BASIS_DIR . 'Public' . DIRECTORY_SEPARATOR . 'Rechnungen' . DIRECTORY_SEPARATOR);
 define('BLOGIC_DIR', BASIS_DIR . 'BLogic' . DIRECTORY_SEPARATOR);
+
+$__tmp__ = NULL;
+$__relativeBasePath__ = NULL;
