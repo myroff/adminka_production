1192cc9ce6fe62e81069fb3244a9d87ba0689c17
adoptimize-as <42072310+adoptimize-as@users.noreply.github.com>
2022-05-26 14:07:07 +0200

    fix configs

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 
 /*** URL CONFIGS */
 define('BASIS_URL', "");
+define('PUBLIC_URL', BASIS_URL . '/Public');
 define('BASIS_RECHNUNG_URL', BASIS_URL."/Public/Rechnungen");
 
 /*** PATH CONFIGS ***/
@@ -11,9 +12,3 @@ define('TWIG_TEMPLATE_DIR', BASIS_DIR . 'TemplatesTwig' . DIRECTORY_SEPARATOR);
 define('BASIS_RECHNUNG_DIR', BASIS_DIR . 'Public' . DIRECTORY_SEPARATOR . 'Rechnungen' . DIRECTORY_SEPARATOR);
 define('BLOGIC_DIR', BASIS_DIR . 'BLogic' . DIRECTORY_SEPARATOR);
 
-/*** DB CONFIGS ***/
-define('DB_HOST', "adminka_db");
-define('DB_PORT', "3306");
-define('DB_NAME', "name");
-define('DB_USER', "user");
-define('DB_PSWD', "pswd");
